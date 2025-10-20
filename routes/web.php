84577d96bb57f8a013296a4a084d3c91b5f3fd35
GissellJV <denia.jimenez@unah.hn>
2025-10-20 12:57:20 -0600

    Se actualizo index

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,6 @@
 
 use App\Http\Controllers\PacienteController;
 use Illuminate\Support\Facades\Route;
-use Laravel\Fortify\Features;
-use Livewire\Volt\Volt;
 use App\Http\Controllers\RutasController;
 
 Route::get('/', [RutasController::class, 'index'])->name('/');
