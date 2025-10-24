55b34bec644de5e685766b6148f1adc75d598b3b
GissellJV <denia.jimenez@unah.hn>
2025-10-24 15:22:27 -0600

    Se agregaron la ruta de informacion de la clinica y tablas de citas, empleados y rol de turnos

---
+++ resources/views/pacientes/informacion_Clinica.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.plantilla')
+@section('contenido')
+
+    <div class="container container-bg-primary mt-5">
+
+        <h1>Vision</h1>
+
+        <p>
+            Ser una clínica médica reconocida a nivel regional por su excelencia en la atención <br>
+            al paciente, innovación en los servicios de salud y compromiso con el desarrollo <br>
+            continuo del personal médico, garantizando una experiencia segura, confiable y de <br>
+            calidad para toda la comunidad. <br>
+        </p>
+        <h1>Mision</h1>
+
+        <p>
+            Brindar servicios médicos integrales de alta calidad, con un enfoque humano, <br>
+            ético y profesional, orientados a la prevención, diagnóstico y tratamiento <br>
+            oportuno de enfermedades, contribuyendo al bienestar y la salud de nuestros <br>
+            pacientes mediante la atención personalizada y el uso de tecnología moderna. <br>
+        </p>
+
+
+    </div>
+@endsection
